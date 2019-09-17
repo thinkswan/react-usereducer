2f2cd409dc1aae1ea07125f374f1c5dff6f4ce9f
Graham Swan <thinkswan@gmail.com>
2019-09-17 11:23:10 +0200

    Add initial filler data to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react"
 
+const allData = new Array(25).fill(0).map((_val, i) => i + 1)
+const perPage = 10
+
 function App() {
-  return <div>Hello world</div>
+  const [data] = React.useState(allData.slice(0, perPage))
+
+  return (
+    <div>
+      <ul>
+        {data.map(row => (
+          <li key={row}>{row}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App
