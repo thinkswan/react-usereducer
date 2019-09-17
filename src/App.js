ab4662acb2d79fa8f61c5589be0cd13a83093628
Graham Swan <thinkswan@gmail.com>
2019-09-17 11:38:05 +0200

    Implement reducer's 'loaded' action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react"
 
+const allData = new Array(25).fill(0).map((_val, i) => i + 1)
+const perPage = 10
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "start":
       return { ...state, loading: true }
     case "loaded":
-      break
+      return {
+        ...state,
+        loading: false,
+        data: [...state.data, ...action.newData],
+        more: action.newData.length === perPage,
+        after: state.after + action.newData.length
+      }
     default:
       throw new Error("Unknown action")
   }
@@ -18,7 +27,7 @@ function App() {
     data: [],
     after: 0
   })
-  const { loading, data } = state
+  const { loading, more, data, after } = state
 
   return (
     <div>
@@ -31,7 +40,15 @@ function App() {
 
         {!loading && (
           <li>
-            <button onClick={() => dispatch({ type: "start" })}>
+            <button
+              onClick={() => {
+                dispatch({ type: "start" })
+
+                const newData = allData.slice(after, after + perPage)
+
+                dispatch({ type: "loaded", newData })
+              }}
+            >
               Load more
             </button>
           </li>
