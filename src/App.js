8106f97d80976168d257752c4489a074e0329855
Graham Swan <thinkswan@gmail.com>
2019-09-17 11:27:46 +0200

    Use reducer to handle data

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import React from "react"
 const allData = new Array(25).fill(0).map((_val, i) => i + 1)
 const perPage = 10
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "start":
+      break
+    case "loaded":
+      break
+    default:
+      throw new Error("Unknown action")
+  }
+}
+
 function App() {
-  const [data] = React.useState(allData.slice(0, perPage))
+  const [state, dispatch] = React.useReducer(reducer, {
+    loading: false,
+    more: true,
+    data: [],
+    after: 0
+  })
 
   return (
     <div>
